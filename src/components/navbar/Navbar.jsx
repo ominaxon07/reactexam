1d8bea3826d7ce879382205f40d43215715df864
ominaxon7 <abobakirovaominaxon@gmail.com>
2025-11-10 15:06:01 +0500

    exam

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { toggleLike, addToCart } from "../../redux/shopSlice";
-import { 
-  FaHeart, FaShoppingCart, FaUserCircle, 
-  FaUser, FaBox, FaTimesCircle, FaStar, FaSignOutAlt, 
-  FaSearch, FaBars, FaTimes 
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaHeart,
+  FaShoppingCart,
+  FaUserCircle,
+  FaUser,
+  FaBox,
+  FaTimesCircle,
+  FaStar,
+  FaSignOutAlt,
+  FaSearch,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
 import "./Navbar.css";
 
-const Navbar = ({ sampleProduct }) => {
-  const liked = useSelector(state => state.shop.liked);
-  const cart = useSelector(state => state.shop.cart);
-  const user = useSelector(state => state.shop.user);
-  const dispatch = useDispatch();
+const Navbar = () => {
+  const liked = useSelector((state) => state.shop.liked);
+  const cart = useSelector((state) => state.shop.cart);
+  const user = useSelector((state) => state.shop.user);
+  const navigate = useNavigate();
 
   const [openMenu, setOpenMenu] = useState(false);
   const [mobileMenu, setMobileMenu] = useState(false);
 
-  const handleLikeClick = () => {
-    dispatch(toggleLike(sampleProduct));
-  };
-
-  const handleCartClick = () => {
-    dispatch(addToCart(sampleProduct));
-  };
+  const goToLikes = () => navigate("/like");
+  const goToCart = () => navigate("/basket");
 
   return (
     <header className="site-header">
       <div className="logo-and-hamburger">
         <h2 className="logo">Exclusive</h2>
+
         <div className="hamburger" onClick={() => setMobileMenu(!mobileMenu)}>
           {mobileMenu ? <FaTimes /> : <FaBars />}
         </div>
@@ -45,22 +48,27 @@ const Navbar = ({ sampleProduct }) => {
       </nav>
 
       <div className="nav-actions">
+        {/* search */}
         <div className="input-wrap">
           <input type="text" placeholder="Search for products..." />
           <button><FaSearch /></button>
         </div>
 
+        {/* icons */}
         <div className="icons">
-          <div className="icon-wrapper" onClick={handleLikeClick}>
+          {/* Likes */}
+          <div className="icon-wrapper" onClick={goToLikes}>
             <FaHeart className="icon" style={{ color: liked.length > 0 ? "red" : "#555" }} />
             {liked.length > 0 && <span className="badge">{liked.length}</span>}
           </div>
 
-          <div className="icon-wrapper" onClick={handleCartClick}>
+          {/* Cart */}
+          <div className="icon-wrapper" onClick={goToCart}>
             <FaShoppingCart className="icon" style={{ color: cart.length > 0 ? "red" : "#555" }} />
             {cart.length > 0 && <span className="badge">{cart.length}</span>}
           </div>
 
+          {/* User */}
           <div className="icon-wrapper" onClick={() => setOpenMenu(!openMenu)}>
             <FaUserCircle className="icon" />
           </div>
